536c7c82d8a781f3ab3f2b4fc8c38633707ccaeb
Joe Fioti <jafioti@gmail.com>
2022-12-05 21:17:50 -0600

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 use axum::{routing::get, Router};
-use std::{net::SocketAddr};
+use std::net::SocketAddr;
 
-pub fn run() -> axum::Server<hyper::server::conn::AddrIncoming, axum::routing::IntoMakeService<Router<(), axum::body::Body>>> {
+pub fn run() -> axum::Server<
+    hyper::server::conn::AddrIncoming,
+    axum::routing::IntoMakeService<Router<(), axum::body::Body>>,
+> {
     let app = Router::new()
         .route("/", get(root))
         .route("/health_check", get(health_check));
-    axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 8000)))
-        .serve(app.into_make_service())
+    axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 8000))).serve(app.into_make_service())
 }
 
 async fn root() -> &'static str {
